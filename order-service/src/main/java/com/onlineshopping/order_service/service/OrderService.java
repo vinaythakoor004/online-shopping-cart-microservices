d584231087d37e7042b46e4bd804830fbc8d714c
vinaythakoor004 <thakoorvinay04@gmail.com>
2025-09-17 15:42:17 +0530

    Added discovery-server and client server configuration in all services

--- order-service/src/main/java/com/onlineshopping/order_service/service/OrderService.java
+++ order-service/src/main/java/com/onlineshopping/order_service/service/OrderService.java
@@ -21,7 +21,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -38,8 +38,8 @@ public class OrderService {
                 .map(OrderLineItems::getSkuCode).toList();
 
         // call inventory service to check if product is in stock or not
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                        .uri("http://localhost:8086//api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                        .uri("http://inventory-service//api/inventory",
                                 uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes).build())
                                 .retrieve()
                                         .bodyToMono(InventoryResponse[].class)
